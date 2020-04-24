30f8c30a3e4e5c5934e1f307ab5dd8d9a2a081a7
Frank Krix <burl410@gmail.com>
2020-04-24 16:18:40 +0200

    add command character

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const getJSON = require('get-json');
 /* variables */
 const client = new DiscordInc.Client();
 const prefix = "+";
-const DISCORD_TOKEN = 'YOUR DISCORD TOKEN HERE';
-const BATTLENET_TOKEN = 'YOUR BATTLENET TOKEN HERE';
+const DISCORD_TOKEN = 'TOKEN DISCORD';
+const BATTLENET_TOKEN = 'BATLLENET';
 
 /* when the bot is ready */
 client.on('ready', () => {
@@ -28,21 +28,10 @@ client.on('message', async message => {
     const command = args.shift().toLowerCase(); //
 
     /* COMMANDE ACCOUNT */
-    if(command == 'account'){
+    if(command == 'profile'){
         if(args[0] != null && args[1] != null){
-            getJSON('https://eu.api.blizzard.com/d3/profile/' + args[0] + '%23' + args[1] + '/?locale=fr_FR&access_token= ' + BATTLENET_TOKEN , function(error, response){
+            getJSON('https://eu.api.blizzard.com/d3/profile/' + args[0] + '%23' + args[1] + '/?locale=fr_FR&access_token=' + BATTLENET_TOKEN , function(error, response){
                 if(response != null){
-
-                    /* Function for give the number of charaters on account */
-                    function getCharacter(){
-                        var count = 0;
-                        for(var i = 0; i < response.heroes.length; i++){
-                            if(response.heroes[i].id =! null){
-                                count ++;
-                            }
-                        }
-                        return count;
-                    }
 
                     /* Embed menu message */
                     const menuEmbed = new DiscordInc.MessageEmbed()
@@ -51,7 +40,7 @@ client.on('message', async message => {
                     .addFields(
                         {name: '**Informations section**', value : 'Account name : ' +  response.battleTag
                         + '\n Guild Name : ' + response.guildName
-                        + '\n Number of characters : ' + getCharacter() },
+                        + '\n Number of characters : ' + response.heroes.length },
 
                         {name: '**Paragon section**', value: 'Total Paragon level: ' + response.paragonLevel 
                         + '\n Hardcore Paragon : '  + response.paragonLevelHardcore
@@ -82,7 +71,45 @@ client.on('message', async message => {
         }
     }
 
-
+    /* COMMANDE ACCOUNT */
+    if(command == 'character'){
+        if(args[0] != null && args[1] != null){
+            getJSON('https://eu.api.blizzard.com/d3/profile/' + args[0] + '%23' + args[1] + '/?locale=en_US&access_token=' + BATTLENET_TOKEN , function(error, response){
+                if(response != null){
+                    /* Embed menu message */
+                    const menuEmbed = new DiscordInc.MessageEmbed()
+                    .setColor('#EFFF00')
+                    .setAuthor(response.battleTag + ' account', 'https://lh3.googleusercontent.com/proxy/lq-Vfl07B2FhkgiKv7oxzCbB2u2gu4PIKkUCbbzcTwSKXOdfEoo2xD_gsqwlEznFI8wz_j2--__-6LmK556F3lTidedTz7UC-0q4uiw_4XjON9oQnv0')
+                    .setDescription('Your have ' + response.heroes.length + ' characters on your account');
+                    for(var i = 0; i < response.heroes.length; i++){
+                        menuEmbed.addField('**Character '+ parseInt(i+1) + '**', 'Character ID : ' + response.heroes[i].id
+                        + '\n Character name : ' + response.heroes[i].name
+                        + '\n Character class : ' + response.heroes[i].class
+                        + '\n Character level : ' + response.heroes[i].level
+                        + '\n Character paragon : ' + response.heroes[i].paragonLevel
+                        + '\n Elites killed : ' + response.heroes[i].kills.elites
+                        + '\n Seasonal character ?  : ' + response.heroes[i].seasonal);
+                        
+                    }
+                    message.channel.send(menuEmbed);
+                    //message.channel.send('Paragon level : ' + response.paragonLevel);
+                }else{
+                    const errorAccountEmbed = new DiscordInc.MessageEmbed()
+                    .setTitle('Account error')
+                    .setDescription('The account does not exist, check the syntax')
+                    .setColor('#ff0000');
+                    message.channel.send(errorAccountEmbed);
+                }
+                
+            });
+        }else{
+            const syntaxErrorEmbed = new DiscordInc.MessageEmbed()
+                    .setTitle('Syntax error')
+                    .setDescription('**Syntax** : +account __<name of account>__ __<battletag without #>__')
+                    .setColor('#ff0000');
+                    message.channel.send(syntaxErrorEmbed);
+        }
+    }
 });
 
 /* TOKENNNNNNNNNNNNN */
